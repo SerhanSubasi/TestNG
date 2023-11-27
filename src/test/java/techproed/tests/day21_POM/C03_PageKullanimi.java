26ed6e4947cb1205764e89174d693768ea5fa80c
Serhan Subaşı <subasiserhan@gmail.com>
2023-11-27 13:33:44 +0300

    21-5

--- src/test/java/techproed/tests/day21_POM/C03_PageKullanimi.java
+++ src/test/java/techproed/tests/day21_POM/C03_PageKullanimi.java
@@ -1,22 +1,30 @@
 package techproed.tests.day21_POM;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import techproed.pages.OpenSourcePage;
 import techproed.utilities.Driver;
 
 public class C03_PageKullanimi {
 
     @Test
-    public void test01() {
+    public void test01() throws InterruptedException {
         //https://opensource-demo.orangehrmlive.com/web/index.php/auth/login adrese gidelim
         Driver.getDriver().get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
         //kullaniciAdi, kullaniciSifre, submitButton elementlerini locate edelim login olalım
+        OpenSourcePage openSourcePage = new OpenSourcePage();
 
         //kullanici=Admin
-
         //kullaniciSifre=admin123
+        openSourcePage.userName.sendKeys("Admin");
+        openSourcePage.password.sendKeys("admin123");
+        openSourcePage.submitButton.click();
+
+        Thread.sleep(3000);
 
         //Login Testinin basarili oldugunu test edelim
+        Assert.assertTrue(openSourcePage.dashBoard.isDisplayed());
 
         //sayfayı kapatalim
 
