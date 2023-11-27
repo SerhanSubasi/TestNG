96e399a7776ad21b509df2acd25153495b7a9523
Serhan Subaşı <subasiserhan@gmail.com>
2023-11-27 11:55:04 +0300

    21-2

--- src/test/java/techproed/tests/day21_POM/C01_DriverIlkTest.java
+++ src/test/java/techproed/tests/day21_POM/C01_DriverIlkTest.java
@@ -6,7 +6,12 @@ import techproed.utilities.Driver;
 public class C01_DriverIlkTest {
     @Test
     public void testtest01() {
+
         Driver.getDriver().get("https://amazon.com");
+        Driver.getDriver().get("https://facebook.com");
+        Driver.getDriver().get("https://google.com");
     }
+
+
 
 }
--- src/test/java/techproed/utilities/Driver.java
+++ src/test/java/techproed/utilities/Driver.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class Driver {
-static WebDriver driver;
+static WebDriver driver;  //driver singleton olmalı, biricik olmalı. o yüzden static.
 
     /*
         Page Object Model'de driver için TestBase classına extends yaparak kullanmak yerine, Driver classı oluşturularak bu classtan
@@ -14,14 +14,23 @@ static WebDriver driver;
          */
 
     public static WebDriver getDriver () {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        return driver;
 
+if (driver==null) {
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+}
+        return driver;
     }
 
+/*
+Driver'ı her çağırdığımızda yeni bir pencere açmaması için bir if block oluşturduk.
 
+if(driver==null) ile eğer driver'a değer atanmamış ise driver'ı başlat dedik. Driver açıkken tekrar çağırılırsa
+driver'a değer atanmış olduğu için if block çalışmayacak ve mevcut driver'ı tekrar return edecek. Böylece aynı driver üzerinden
+test senaryolarımıza devam edebileceğiz.
+
+ */
 
 
 }
