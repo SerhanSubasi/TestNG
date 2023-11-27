2be86fa8caa7add57c196dd37d98471c4b8b6cf0
Serhan Subaşı <subasiserhan@gmail.com>
2023-11-27 13:55:42 +0300

    21-6

---
+++ src/test/java/techproed/utilities/ConfigReader.java
@@ -0,0 +1,29 @@
+package techproed.utilities;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    public static String getProperty(String key) {
+
+        Properties properties = new Properties(); //ilk iş obje oluşturmak
+
+        try {
+            FileInputStream fis = new FileInputStream("configuration.properties");
+            properties.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return properties.getProperty(key);
+
+
+    }
+
+
+
+}
